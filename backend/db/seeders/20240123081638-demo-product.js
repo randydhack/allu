4c6f37aabf271aacd3f858a5a7f303032c5c27af
Kian Seyedjafari <kian.matthew@berkeley.edu>
2024-01-23 00:27:07 -0800

    demo product data

---
+++ backend/db/seeders/20240123081638-demo-product.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Products';
+   return queryInterface.bulkInsert(options, [
+    {
+      name: "T-Shirt",
+      colors: "red-green-blue",
+      size: "xxs-xs-s-m-l-xl-xxl",
+
+    },
+    {
+      name: "Sweater",
+      colors: "red-yellow-orange-white-black",
+      size: "s-m-l-xl-xxl",
+    },
+    {
+      name: "Longsleeve",
+      colors: "blue-black-white-gray",
+      size: "xs-s-m-l-xl",
+    },
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Products';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: {
+        [Op.in]: ['T-Shirt', 'Sweater', 'Longsleeve']
+      }
+    }, {});
+  }
+};
