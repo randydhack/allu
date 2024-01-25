81853797c1d8e7845f4425308c2be2a1b301b1fc
randy hac <randydhacks@gmail.com>
2024-01-25 00:06:51 -0800

    can get users order by id

--- backend/routes/api/orders.js
+++ backend/routes/api/orders.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const { Order, Batch } = require("../../db/models");
+const { Op } = require("sequelize");
 
 const { requireAuth } = require("../../utils/auth");
 
+
+// Fetches all orders for current user. NOTE: try to remove the null values form the batches
 router.get("/", requireAuth, async (req, res, next) => {
-    const { user } = req
+  const { user } = req;
   const orders = await Order.findAll({
-    where: { userId: user.id},
+    where: { userId: user.id },
     include: {
-        model: Batch
-    }
-  }
-  );
+      model: Batch,
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    },
+  });
 
   res.status(200).json(orders);
 });
 
+
 router.get("/:orderId", async (req, res, next) => {
-  const order = await Order.findByPk(req.params.orderId);
+    const { user } = req;
+    const { orderId } = req.params
+  const order = await Order.findOne({
+    where: {userId: user.id, id: orderId},
+    include: {
+        model: Batch,
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    }
+  });
 
   if (!order) {
     const err = new Error("Order does not exist");
