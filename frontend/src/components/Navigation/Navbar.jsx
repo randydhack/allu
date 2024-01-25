a7fd08119d36ba5c74652a092b353d1d690c3263
Dan Holodak <danholodak@gmail.com>
2024-01-25 14:52:35 -0500

    Update nav bar and add logos and favicons

--- frontend/src/components/Navigation/Navbar.jsx
+++ frontend/src/components/Navigation/Navbar.jsx
@@ -1,7 +1,6 @@
 // Libaries
 import {useContext} from 'react'
 import { NavLink } from 'react-router-dom'
-
 // Context
 import { ModalContext } from '../../context/modalContext'
 
@@ -18,19 +17,20 @@ function Navbar() {
 
   return (
     <div className='nav__container'>
-        <div>
-            LOGO
-        </div>
+        <NavLink to="/">
+            <img className="nav_logo" src="t_shirt_logo.png" alt="all-u logo, click to return to home page" />
+        </NavLink>
 
         <div className='nav__middle__section'>
-            <NavLink>Our Designs</NavLink>
+            <NavLink to="/our-designs">Our Designs</NavLink>
             <NavLink>Create your own designs</NavLink>
             <NavLink>About us</NavLink>
         </div>
 
         <div className='nav__right__section'>
             <button onClick={() => toggleLogin()}>Sign in</button>
-            <div>ICON</div>
+            <button onClick={() => toggleSignUp()}>Register</button>
+            <button><i className="fa-solid fa-cart-shopping"></i></button>
         </div>
     </div>
   )
