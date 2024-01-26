5eace596d77efa13f11ecc71731e3ead7a83c9de
randy hac <randydhacks@gmail.com>
2024-01-25 17:18:18 -0800

    asdsad

--- backend/routes/api/orders.js
+++ backend/routes/api/orders.js
@@ -5,7 +5,6 @@ const { Op } = require("sequelize");
 
 const { requireAuth } = require("../../utils/auth");
 
-
 // Fetches all orders for current user. NOTE: try to remove the null values form the batches
 router.get("/", requireAuth, async (req, res, next) => {
   const { user } = req;
@@ -22,18 +21,18 @@ router.get("/", requireAuth, async (req, res, next) => {
   res.status(200).json(orders);
 });
 
-
-router.get("/:orderId", async (req, res, next) => {
-    const { user } = req;
-    const { orderId } = req.params
+// Get user's order by order id
+router.get("/:orderId", requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const { orderId } = req.params;
   const order = await Order.findOne({
-    where: {userId: user.id, id: orderId},
+    where: { userId: user.id, id: orderId },
     include: {
-        model: Batch,
+      model: Batch,
       attributes: {
         exclude: ["createdAt", "updatedAt"],
       },
-    }
+    },
   });
 
   if (!order) {
@@ -43,6 +42,28 @@ router.get("/:orderId", async (req, res, next) => {
   }
 
   res.status(200).json(order);
+});
+
+// Create an order
+router.post("/", requireAuth, async (req, res, next) => {
+
+  const { address } = req.body;
+
+  const order = await Order.create(address);
+
+  const cart = await cart.findOne({ where: { userId: user.id }, include: { method: Batch } });
+
+  for (let i = 0; i < cart.Batch.length; i++) {
+
+    const curr = await Batch.findByPk(cart.Batch[i].id);
+
+    await curr.update({
+        orderId: order.id,
+        cartId: null
+    })
+  }
+
+  res.status(200).json
 });
 
 module.exports = router;
