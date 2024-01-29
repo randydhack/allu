d6cbab2cee6d93e8e360b5f2a3bda7a5c36848db
Joyce <joyceyukang@gmail.com>
2024-01-29 12:34:07 -0800

    seeder syntax

--- backend/db/seeders/20240123090155-demo-user-design.js
+++ backend/db/seeders/20240123090155-demo-user-design.js
@@ -13,22 +13,22 @@ module.exports = {
       {
         id: 1,
         img_url: "https://content-management-files.canva.com/cdn-cgi/image/f=auto,q=70/2fdbd7ab-f378-4c63-8b21-c944ad2633fd/header_t-shirts2.jpg",
-        userid: 2
+        userId: 2
       },
       {
         id: 2,
         img_url: "https://cdn.logojoy.com/wp-content/uploads/20220316180959/event-t-shirt-design-ideas-600x600.jpg",
-        userid: 3
+        userId: 3
       },
       {
         id: 3,
         img_url: "https://files.cdn.printful.com/o/upload/bfl-image/bc/10350_l_support%20local%20businesses.jpg",
-        userid: 4
+        userId: 4
       },
       {
         id: 4,
         img_url: "https://i.pinimg.com/474x/a1/7e/23/a17e23192907c8bfed8d6cb6923107e0.jpg",
-        userid: 5
+        userId: 5
       },
      ])
   },
