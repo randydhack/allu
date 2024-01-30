aa6a951b7bb0d7bda1e5d9e9a5753265aa45aa86
Joyce <joyceyukang@gmail.com>
2024-01-29 22:23:18 -0800

    updated design seeders

--- backend/db/seeders/20240123090127-demo-design.js
+++ backend/db/seeders/20240123090127-demo-design.js
@@ -13,31 +13,175 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         id: 1,
-        design_url: "https://www.uberprints.com/assets/images/templates/cat/boxing.png",
+        design_url: "allu/backend/db/designs/aventura_1.png",
         text_layers: 2,
         design_price: 1.00
       },
       {
         id: 2,
-        design_url: "https://www.uberprints.com/assets/images/templates/cat/bowling.png",
+        design_url: "allu/backend/db/designs/aventura_2.png",
         text_layers: 3,
         design_price: 2.00
       },
       {
         id: 3,
-        design_url: "https://www.uberprints.com/assets/images/templates/cat/biking.png",
+        design_url: "allu/backend/db/designs/aventura_3.png",
         text_layers: 1,
         design_price: 0.50
       },
       {
         id: 4,
-        design_url: "https://www.uberprints.com/assets/images/templates/cat/bowling.png",
+        design_url: "allu/backend/db/designs/aventura_4.png",
         text_layers: 0,
         design_price: 0.00
       },
       {
         id: 5,
-        design_url: "https://www.uberprints.com/assets/images/templates/cat/cheerleading.png",
+        design_url: "allu/backend/db/designs/aventura_5.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 6,
+        design_url: "allu/backend/db/designs/aventura_6.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 7,
+        design_url: "allu/backend/db/designs/aventura_7.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 8,
+        design_url: "allu/backend/db/designs/aventura_8.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 9,
+        design_url: "allu/backend/db/designs/aventura_9.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 10,
+        design_url: "allu/backend/db/designs/aventura_10.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 11,
+        design_url: "allu/backend/db/designs/aventura_11.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 12,
+        design_url: "allu/backend/db/designs/aventura_12.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 13,
+        design_url: "allu/backend/db/designs/aventura_13.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 14,
+        design_url: "allu/backend/db/designs/aventura_14.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 15,
+        design_url: "allu/backend/db/designs/aventura_15.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 16,
+        design_url: "allu/backend/db/designs/aventura_17.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 17,
+        design_url: "allu/backend/db/designs/aventura_18.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 18,
+        design_url: "allu/backend/db/designs/aventura_19.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 19,
+        design_url: "allu/backend/db/designs/aventura_20.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 20,
+        design_url: "allu/backend/db/designs/aventura_21.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 21,
+        design_url: "allu/backend/db/designs/aventura_22.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 22,
+        design_url: "allu/backend/db/designs/aventura_23.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 23,
+        design_url: "allu/backend/db/designs/aventura_24.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 24,
+        design_url: "allu/backend/db/designs/aventura_25.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 25,
+        design_url: "allu/backend/db/designs/aventura_26.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 26,
+        design_url: "allu/backend/db/designs/aventura_27.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 27,
+        design_url: "allu/backend/db/designs/aventura_28.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 28,
+        design_url: "allu/backend/db/designs/aventura_29.png",
+        text_layers: 2,
+        design_price: 1.00
+      },
+      {
+        id: 29,
+        design_url: "allu/backend/db/designs/aventura_30.png",
         text_layers: 2,
         design_price: 1.00
       },
@@ -49,7 +193,7 @@ module.exports = {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       id: {
-        [Op.in]: [1, 2, 3, 4, 5]
+        [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
       }
     }, {});
   }
