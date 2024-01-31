a7b5a01c989ed095db83b4d02c9f5a883c2f2d8f
randy hac <randydhacks@gmail.com>
2024-01-30 19:40:36 -0800

    design cart button

--- frontend/src/components/OurDesigns/OurDesigns.jsx
+++ frontend/src/components/OurDesigns/OurDesigns.jsx
@@ -33,13 +33,16 @@ function OurDesigns() {
   return isLoaded && (
       <div className="design_main">
       <div className="design_image_container">
+        <div className='design_headings'>
+          <h1>Designs</h1>
+          <h2>All designs ({designs.Designs.length})</h2>
+        </div>
         <div className="design_grid">
           {designs.Designs.map((el, i) => {
             return (
               <div key={`design${i}`}>
                 <NavLink to={`/design/${el.id}/product`}>
-                <img src={el.design_url} alt="image" className="design_image" />
-
+                <img src={el.design_url} alt={`design-${el.id}`} className="design_image" />
                 </NavLink>
               </div>
             );
