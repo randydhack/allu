06b0a54ca0f48abab53a508507514fb31a75e25e
Joyce <joyceyukang@gmail.com>
2024-01-30 20:38:14 -0800

    seeeder attempt

--- backend/db/seeders/20240123082811-demo-product-image.js
+++ backend/db/seeders/20240123082811-demo-product-image.js
@@ -1,5 +1,7 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+const { hoodieSeeder } = require("../seederScripts");
+const { undoHoodie } = require("../seederScripts");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
@@ -10,26 +12,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName="ProductImages";
-    return queryInterface.bulkInsert(options, [
-      {
-        productId: 1,
-        img_url: "https://cdnp.sanmar.com/medias/sys_master/images/images/hbc/h92/10234018922526/9671-Crunchberry-1-1717CrunchberryModelFront-337W.jpg",
-        description: "Basic shirt made of cotton. Soft-washed, garment-dyed fabric.",
-        color: "pink",
-      },
-      {
-        productId: 2,
-        img_url: "https://cdnp.sanmar.com/medias/sys_master/images/images/h14/h0a/11351843831838/115-HtSptRoyal-1-18500HtSptRoyalModelFront-337W.jpg",
-        description: "Basic sweater made of cotton. Spandex material. Front pouch pocket.",
-        color: "blue",
-      },
-      {
-        productId: 3,
-        img_url: "https://cdnp.sanmar.com/medias/sys_master/images/images/he9/hef/10232928534558/790-Brick-1-6014BrickModelFront-337W.jpg",
-        description: "Longsleeve top. Rib knit cuffs. Relaxed fit.",
-        color: "red",
-      },
-     ], {})
+    return queryInterface.bulkInsert(options, hoodieSeeder(), {})
   },
 
   async down (queryInterface, Sequelize) {
@@ -37,7 +20,7 @@ module.exports = {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       productId: {
-        [Op.in]: [1, 2, 3]
+        [Op.in]: undoHoodie()
       }
     }, {});
   }
