f53b5a7a4c31f12fb18343e1849bb725575ae1e4
Joyce <joyceyukang@gmail.com>
2024-01-30 23:19:46 -0800

    so many edits

--- backend/db/models/userdesign.js
+++ backend/db/models/userdesign.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       UserDesign.belongsTo(models.User, { foreignKey: 'userId' })
-      UserDesign.belongsTo(models.Batch, { foreignKey: 'userDesignId' })
+      UserDesign.hasMany(models.Batch, { foreignKey: 'userDesignId' })
     }
   }
   UserDesign.init({
